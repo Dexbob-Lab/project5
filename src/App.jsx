3d5bc2639adaf1c8be6dfdb056d848e972e8b506
dexbob <mimacro@naver.com>
2024-10-23 16:07:51 +0900

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<h1>지리산 단감 레이스</h1>
+			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+		</>
+	);
+}
+
+export default App;
