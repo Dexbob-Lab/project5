59ea34ed3554a99bed8aa20e209ec72670e5482f
dexbob <mimacro@naver.com>
2024-10-27 23:44:51 +0900

    참가신청 화면 생성

---
+++ src/components/Register.jsx
@@ -0,0 +1,90 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export default function Register() {
+	const ref_form = useRef(null);
+	const ref_elements = useRef([]);
+
+	const resetForm = () => {
+		ref_elements.forEach(dom => (dom.current.value = ''));
+	};
+
+	const sendForm = e => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, ref_form.current, {
+				publicKey: import.meta.env.VITE_PUBLIC_KEY
+			})
+			.then(res => {
+				alert('등록되었습니다.');
+				console.log(res);
+				resetForm();
+			})
+			.catch(err => {
+				alert('에러 발생!\n' + err);
+				console.log(err);
+			});
+	};
+	let cnt = 0;
+	const setRef_elements = dom => (ref_elements.current[cnt++] = dom);
+	return (
+		<div className='register'>
+			<form ref={ref_form} onSubmit={sendForm}>
+				<span>
+					<label htmlFor='uName'>이름(Name)</label>
+					<input ref={setRef_elements} type='text' name='user_name' id='uName' placeholder='Leave your name' />
+				</span>
+				<span>
+					<label htmlFor='uBirth'>생년월일(Date of Birth)</label>
+					<input ref={setRef_elements} type='date' name='user_birth' id='uBirth' />
+				</span>
+				<span>
+					<label htmlFor='uPhone'>연락처(Phone Number)</label>
+					<input ref={setRef_elements} type='tel' name='user_phone' id='uPhone' placeholder='Leave your phone number' />
+				</span>
+				<span>
+					<label htmlFor='uAddress'>주소(Address)</label>
+					<input ref={setRef_elements} type='text' name='user_address' id='uAddress' placeholder='Leave your address' />
+				</span>
+				<span>
+					<label htmlFor='uMail'>이메일(e-mail)</label>
+					<input ref={setRef_elements} type='email' name='user_email' id='uMail' placeholder='Leave your email' />
+				</span>
+				<span>
+					<label htmlFor='uPassword'>비밀번호(password)</label>
+					<input
+						ref={setRef_elements}
+						type='password'
+						name='user_password'
+						id='uPassword'
+						placeholder='Leave your password'
+					/>
+				</span>
+				<span>
+					<label htmlFor='sSize'>옷사이즈(shirt-size)</label>
+					Radio
+					<input ref={setRef_elements} type='radio' name='shirt_size' id='sSize' value='S' />
+				</span>
+
+				<nav className='btnSet'>
+					<input type='reset' value='Cancel' />
+					<input type='submit' value='Send' />
+				</nav>
+			</form>
+			<div className='info'>
+				<h2>Information</h2>
+				<ul>
+					<li>2024년 지리산 숲길 단감레이스 21.3km 참가 신청서 입니다. 참가비용은 59,000원 입니다.</li>
+					<li>허위 작성 시 참가 신청이 취소 될 수 있습니다.</li>
+					<li>
+						이름, 생년월일 6자리, 연락처, 상세주소, 성별, 이메일을 꼭 기입 해 주시길 바랍니다.(잘못기제, 미기제 시
+						기념품 및 사진 수령이 어려울 수 있습니다)
+					</li>
+					<li>입금 계좌 : 농협 301-0270-6460-51 / 지리산씨 협동조합</li>
+					<li>참가 신청 내역과 입금 확인 후 3일 이내 참가 신청 확정 안내 문자 드립니다.</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
--- src/pages/Application.jsx
+++ src/pages/Application.jsx
@@ -1,11 +1,13 @@
 import Layout from '../layouts/Layout';
 import useGlobalData from '../hooks/useGlobalData';
+import Register from '../components/Register';
 
 export default function Application() {
 	const { Lang, CurrObject } = useGlobalData();
 	return (
 		<Layout className={CurrObject?.name}>
 			<p>{CurrObject?.text[Lang]}</p>
+			<Register />
 		</Layout>
 	);
 }
