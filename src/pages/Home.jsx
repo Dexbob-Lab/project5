58ad819fd5a75662fa58a4577c5d691ef9295e55
dexbob <mimacro@naver.com>
2024-11-01 15:10:54 +0900

    홈화면 약간의 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Layout from '../layouts/Layout';
 import useGlobalData from '../hooks/useGlobalData';
+import Visual from '../components/Visual';
 
 export default function Home() {
 	const { Lang, CurrObject } = useGlobalData();
 	return (
 		<Layout className={CurrObject?.name}>
 			<h1>{CurrObject?.text[Lang]}</h1>
+			<Visual />
 		</Layout>
 	);
 }
