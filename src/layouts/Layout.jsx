57b8e689e23a35f4d8f3543582e8b7dfd066fb1f
dexbob <mimacro@naver.com>
2024-11-07 18:38:08 +0900

    스크롤시 위로가기 버튼 생성

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+import { throttle } from 'lodash';
+import { FaCircleUp } from 'react-icons/fa6';
+
 export default function Layout({ className, children }) {
+	const [Visible, setVisible] = useState(false);
+
+	useEffect(() => {
+		window.addEventListener('scroll', throttleGoTopBtn);
+		return () => {
+			window.removeEventListener('scroll', throttleGoTopBtn);
+		};
+	}, []);
+	const throttleGoTopBtn = throttle(() => (window.scrollY > 0 ? setVisible(true) : setVisible(false)), 300);
+
+	const handleScroll = () => {
+		if (!window.scrollY) return;
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	};
+
 	return (
 		<main className={className}>
 			<section>{children}</section>
+			{Visible && (
+				<button id='goTopBtn' onClick={handleScroll}>
+					<FaCircleUp />
+				</button>
+			)}
 		</main>
 	);
 }
